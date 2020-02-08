79c7c6f9d230f5334db550aaf63b838106ec841c
dr1nky <leedrinkwater@hotmail.com>
2020-02-08 14:57:10 +0000

    commit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,9 @@
 const menuButton = document.querySelector(".toggle");
 const menuItems = document.querySelectorAll(".item");
-
-// menuButton.addEventListener('click', () => {
-  //   for (var i = 0; i < menuItems.length; i++) {
-    //     menuItems[i].classList.toggle('active');
-    //   }
-    // });
-    
-    menuButton.addEventListener('click', () => {
-      for (let i = 0; i < menuItems.length; i++) {
-        menuItems[i].toggleAttribute('active');
-      }
-    });
-    
-    // menuButton.addEventListener('click', () => {
-      //   menuButton.classList.toggle('active');
-      // });
-      
-      menuButton.addEventListener('click', () => {
-        menuButton.toggleAttribute('active');
-      });
+  
+menuButton.addEventListener('click', () => {
+  menuButton.toggleAttribute('active');
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].toggleAttribute('active');
+  }
+});
