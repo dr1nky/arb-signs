1099cc660e30a90996591d085b0bc5fb6e36096d
dr1nky <leedrinkwater@hotmail.com>
2020-02-09 15:29:11 +0000

    commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-const menuButton = document.querySelector(".toggle");
-const menuItems = document.querySelectorAll(".item");
-  
+const menuButton = document.querySelector(".menu-icon");
+const menuItems = document.querySelectorAll(".navigation");
+const overlayPanel = document.querySelector(".overlay");
+
 menuButton.addEventListener('click', () => {
   menuButton.toggleAttribute('active');
+  overlayPanel.toggleAttribute('active');
   for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].toggleAttribute('active');
   }
